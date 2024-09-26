15761a3d12a95c06b1231ab128e5d250a6325b8b
VicMelia <al415395@uji.es>
2024-09-26 10:23:29 +0200

    Player movement done

--- IAProject/Assets/Scripts/Player/PlayerMovement.cs
+++ IAProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    //Base movement
     public float turnSpeed = 20f;
     public float moveSpeed = 5f; // Speed at which the player moves
 
+    public float runSpeed = 7f; // Sprint speed
+
+    private float currentSpeed; //Final speed (normal or sprint)
     private Rigidbody m_Rigidbody;
     private Vector3 m_Movement;
 
+
+    //Ground detection
+    private float m_RaycastOffset = 0.5f;
+
+    private Vector3 m_RaycastOrigin; 
+    public LayerMask groundLayer;
+    private bool grounded;
+
+    //Jump
+    private bool canJump; //Fixes double jump bug
+
+    public float jumpForce = 5f;
+
+    private float jumpCooldownTime = 0.5f;
+
     void Start()
     {
+        InitializeVariables();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Get movement input from W, A, S, D keys
-        float horizontal = Input.GetAxis("Horizontal"); // A and D
-        float vertical = Input.GetAxis("Vertical"); // W and S
+    
+        float horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical"); 
 
-        // Create the movement vector
+        //Movement Vector
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
+
+        //Jump
+        if(!grounded) grounded = CheckGrounded();
+        else if (grounded && canJump && Input.GetButtonDown("Jump")) Jump();
+
+        //Run
+        currentSpeed = Input.GetKey(KeyCode.LeftShift)? runSpeed : moveSpeed;
+            
     }
 
     void FixedUpdate()
@@ -31,12 +60,42 @@ public class PlayerMovement : MonoBehaviour
         if (m_Movement.magnitude > 0)
         {
             // Move the player using the movement vector multiplied by speed
-            Vector3 targetPosition = m_Rigidbody.position + m_Movement * moveSpeed * Time.fixedDeltaTime;
+            Vector3 targetPosition = m_Rigidbody.position + m_Movement * currentSpeed * Time.fixedDeltaTime;
+            //m_Rigidbody.velocity = new Vector3(m_Movement.x, 0f, m_Movement.z);
             m_Rigidbody.MovePosition(targetPosition);
 
             // Calculate the desired forward direction and rotation
             Quaternion targetRotation = Quaternion.LookRotation(m_Movement);
             m_Rigidbody.MoveRotation(Quaternion.Slerp(m_Rigidbody.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime));
         }
+    }
+
+    void Jump(){
+        grounded = false;
+        canJump = false;
+        m_Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        StartCoroutine(JumpCooldown(jumpCooldownTime));
+
+    }
+
+    IEnumerator JumpCooldown(float c){
+
+        yield return new WaitForSeconds(c);
+        canJump = true;
+
+    }
+
+    bool CheckGrounded(){
+
+        RaycastHit hit;
+        m_RaycastOrigin = transform.position;
+        m_RaycastOrigin.y += m_RaycastOffset;
+        return Physics.SphereCast(m_RaycastOrigin, 0.5f, Vector3.down, out hit, 1f, groundLayer);
+    }
+
+    void InitializeVariables(){
+
+        grounded = true;
+        canJump = true;
     }
 }
