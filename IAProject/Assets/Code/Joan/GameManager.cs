921d5d9a6ba7f16ee22972dd3d409ba33a219f10
Joan Castells Appriou <al416685@uji.es>
2024-10-08 16:24:12 +0200

    GM actualizado

--- IAProject/Assets/Code/Joan/GameManager.cs
+++ IAProject/Assets/Code/Joan/GameManager.cs
@@ -2,28 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    //int state = -1
-    //int selLevel = -1
+public class GameManager : MonoBehaviour {
+    int state = -1;
+    int selLevel = -1;
     GameObject[] levels;
     int liveChickens = 0;
+    bool caught = false;
+    bool victory = false;
 
-    void Start()
-    {
-        
+    void Start() {}
+
+    void Update() {
+
+        switch (state) {
+            case 0: startGame(); break;
+            case 1: runGame(); break;
+            case 2: endGame(); break;
+        }
     }
 
-    
-    void Update()
-    {
+    public void statCheck() {
 
-        /*
-        switch (state) {
-            case 0: StartGame(); break;
-            case 1: RunGame(); break;
-            case 2: EndGame(); break;
+        if (caught) {
+            state = 2;
         }
-        */
+        else if (liveChickens == 0) {
+            victory = true;
+            state = 2;
+        }
+    }
+
+    void startGame() {
+        selLevel = 0;
+        levels[selLevel].SetActive(true);
+    }
+
+    void runGame() {}
+
+    void endGame() {
+        levels[selLevel].SetActive(false);
+
+        if (selLevel < levels.Length - 1 && victory) {
+            selLevel++;
+            victory = false;
+            levels[selLevel].SetActive(true);
+            state = 1;
+        }
+        else {
+            victory = false;
+            state = -1;
+        }
     }
 }
