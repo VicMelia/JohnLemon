f837a68b4492861e2fc5e40a0cd3a59a22f4b585
Alex Arévalo <alex.are.mpf@gmail.com>
2024-10-14 18:44:55 +0200

    Update NPC.cs

--- IAProject/Assets/Scripts/NPC.cs
+++ IAProject/Assets/Scripts/NPC.cs
@@ -5,13 +5,53 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     Node currentNode;
+    float speed;
     public Pathfinding pathfinding;
+
+    public Transform target;
+    Vector3 path;
+    int targetIndex;
 
     private void Start()
     {
         pathfinding = GetComponent<Pathfinding>();
     }
 
+    private void GoTo(Vector3 newPosition)
+    {
 
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+
+        }
+    }
 
 }
