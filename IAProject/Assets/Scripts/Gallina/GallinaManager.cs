6f570ffcf8418310554666a544adc2e37abca0ad
Joan Castells Appriou <al416685@uji.es>
2024-10-14 18:02:14 +0200

    Create GallinaManager.cs

---
+++ IAProject/Assets/Scripts/Gallina/GallinaManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GallinaManager {
+
+    //void Start() {}
+
+    //void Update() {}
+
+    //void scared() {}
+
+}
